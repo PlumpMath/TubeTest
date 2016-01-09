17ca69007a6f088707346e0593676de9ab04f6d8
edapx <lastexxit@gmail.com>
2016-01-09 12:54:34 +0100

    first commit

---
+++ src/ofApp.cpp
@@ -0,0 +1,111 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofSetVerticalSync(true);
+    ofEnableDepthTest();
+    //light
+    light.setPosition(500, 500, 500);
+    light.enable();
+    
+    //camera
+    cam.setPosition(0, -440, 300);
+    cam.lookAt(ofVec3f(0, 0, 20));
+    
+    //plane
+    plane.setPosition(100, 50, 0);
+    plane.set(640, 480);
+    plane.setResolution(20, 20);
+    
+    auto point1 = ofVec3f(0, 0, 0);
+    auto point2 = ofVec3f(0, 0, 100);
+    auto point3 = ofVec3f(100, 0, 200);
+    
+    auto point4 = ofVec3f(0, 0, 100);
+    auto point5 = ofVec3f(-100, 0, 200);
+    
+    container1.push_back(point1);
+    container1.push_back(point2);
+    container2.push_back(point3);
+    container2.push_back(point4);
+    container2.push_back(point5);
+    
+    tube1.setUseVbo(true);
+    tube1.setup(container1, 20);
+    
+    tube2.setUseVbo(true);
+    tube2.setup(container2, 20);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    tube1.update();
+    tube2.update();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofEnableDepthTest();
+    cam.begin();
+    //ofDrawAxis(600.0);
+    plane.drawWireframe();
+    tube1.draw();
+    tube2.draw();
+    cam.end();
+    ofDisableDepthTest();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
